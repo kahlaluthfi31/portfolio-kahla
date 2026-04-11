ccf6206793d4bf86b0805ce7fafd1df54d4966c7
Kahla Luthfiyah Halim <kahlaluthifiyah@gmail.com>
2026-04-12 06:55:11 +0700

    perbaikan url

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -55,7 +55,7 @@ export default function Navbar() {
 
             <nav ref={navRef} className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
 
-                <a href="https://prebuiltui.com?utm_source=eliana">
+                <a href="#">
                     <img src="/assets/logo.png" alt="Logo" className="w-28 cursor-pointer mr-14 dark:hidden" />
                     <img src="/assets/logo_dark.png" alt="Logo" className="w-28 cursor-pointer mr-14 hidden dark:block" />
                 </a>
